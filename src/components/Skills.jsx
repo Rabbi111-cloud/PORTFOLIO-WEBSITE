f461a0ff73e20b9a386eec643ecb3e906c2fb5a8
Rabbi111-cloud <diggingdeep0002@gmail.com>
2026-02-09 18:53:39 +0100

    Create Skills.jsx

---
+++ src/components/Skills.jsx
@@ -0,0 +1,11 @@
+export default function Skills() {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <p>
+        JavaScript, React, Node.js, Express, MongoDB, Firebase, REST APIs,
+        GitHub, Vercel
+      </p>
+    </section>
+  );
+}
